c08c37e072b7087cdef3f5943ad843e604a48e6a
Enzo Reis Francelli <francelli.dev@gmail.com>
2026-01-27 16:08:30 -0300

    Add files via upload

---
+++ Linguagem C#/Projeto_entre_valores_csharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_entre_valores_csharp {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int x, y, troca, soma;
+
+            Console.Write("Digite o primeiro valor: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo numero: ");
+            y = int.Parse(Console.ReadLine());
+
+            if(x > y) {
+                troca = x;
+                x = y;
+                y = troca;
+            }
+
+            soma = 0;
+            for(int i=x+1; i<y; i++) {
+                if(i % 2 != 0) {
+                    soma = soma + i;
+                }
+            }
+
+            Console.Write("\n");
+            Console.Write("A SOMA DOS IMPARES ENTRE OS VALORES APRESENTADOS EH "+soma);
+
+        }
+    }
+}
