e8f320fc381fb669e2123ea68a9541dd2775e7ee
Enzo Reis Francelli <francelli.dev@gmail.com>
2026-01-27 16:14:36 -0300

    Add files via upload

---
+++ Linguagem C#/Projeto_temp_csharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_temp_csharp {
+    internal class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double C, F;
+            char resp;
+
+            do {
+                Console.Write("Digite a temperatura em Celsius: ");
+                C = double.Parse(Console.ReadLine(), CI);
+                F = 9.0 * C / 5.0 + 32.0;
+                Console.WriteLine("Equivalente em Fahrenheit: " +F.ToString("F1", CI));
+                Console.Write("Deseja repetir (s/n)? ");
+                resp = char.Parse(Console.ReadLine());
+            } while (resp == 's');
+
+        }
+    }
+}
