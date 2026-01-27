59fe809a7a39eb9ea4a2cb7fb3de6afb8483e23f
Enzo Reis Francelli <francelli.dev@gmail.com>
2026-01-27 16:10:50 -0300

    Add files via upload

---
+++ Linguagem C#/Projeto_loop1_csharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_loop1_csharp {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int x, soma;
+
+            soma = 0;
+            Console.Write("Digite o primeiro numero: ");
+            x = int.Parse(Console.ReadLine());
+
+            while (x != 0) {
+                soma = soma + x;
+                Console.Write("Digite outro numero: ");
+                x = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("SOMA = " + soma);
+
+        }
+    }
+}
